64f21424ff0a2ebefff4073b1e92304a23d592de
Brendan C. Todahl <btodahl29@gmail.com>
2020-05-24 13:13:56 -0400

    Setup layout and basic components.

---
+++ CookieClicker/components/BuildingList.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav>
+    <h3 class="sidebar-title">Buildings</h3>
+
+    <ul class="building-list list-unstyled">
+      <li>
+        <span>Item 1</span>
+      </li>
+      <li>
+        <span>Item 2</span>
+      </li>
+      <li>
+        <span>Item 3</span>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.sidebar-title {
+  margin: 5px;
+}
+
+.building-list li:first-of-type {
+  border-top: 1px solid black;
+}
+
+.building-list li {
+  height: 3em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid black;
+}
+</style>
---
+++ CookieClicker/components/Cookie.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button type="button" class="btn btn-primary" @click="click">
+      Cookie
+    </button>
+    <div>Cookies: {{ numberOfCookies }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      numberOfCookies: 0
+    }
+  },
+  methods: {
+    click () {
+      this.numberOfCookies++
+    }
+  }
+}
+</script>
+
+<style></style>
